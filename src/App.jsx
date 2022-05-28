f9d294aba7234a9dc53d80ca9df6e0cc5a614100
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-28 15:12:12 -0600

    Creation of files and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/servicies/Services'
+import Portafolio from './components/portafolio/Portafolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 function App() {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portafolio />
+            <Contact />
+            <Footer />
+        </>
     )
 }
 
