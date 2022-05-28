06d35f98be08a89d4e86d950255829c5d4ea0d73
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-28 17:52:59 -0600

    IMG computer header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './header.css'
 import Social from './Social'
+import Laptop from './../../assets/laptop.jpg'
 
 const Header = () => {
   return (
     <header>
       <div className="contenedor h_contenedor">
         <h5></h5>
-        <h1>Stefano Aragoni</h1>
+        <h1>Stefano Aragoni.</h1>
         <h5 className="text-blanco2">Software Developer</h5>
 
         <Social/>
+
+        <div className='Laptop'>
+          <img src={Laptop} alt="Laptop" />
+        </div>
 
       </div>
     </header>
