8111a31a8e4b6d504c4083d7085ca4ad948f61fa
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-28 20:13:49 -0600

    css update

--- src/components/header/Social.jsx
+++ src/components/header/Social.jsx
@@ -6,8 +6,8 @@ import { BsGithub } from 'react-icons/bs'
 const Social = () => {
   return (
     <div className='h_social'>
-        <a href='https://github.com/stefanoaragoni' target={"_blank"}><BsGithub/></a>
-        <a href='https://linkedin.com/in/stefanoaragoni' target={"_blank"}>< BsLinkedin/></a>
+        <a href='https://github.com/stefanoaragoni' target={"_blank"}><BsGithub size={30}/></a>
+        <a href='https://linkedin.com/in/stefanoaragoni' target={"_blank"}>< BsLinkedin size={30}/></a>
     </div>
   )
 }
