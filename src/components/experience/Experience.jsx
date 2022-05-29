862acfab29114a6725b910a4797e971e0b93ec96
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-29 16:16:12 -0600

    experience inicial

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
 import './experience.css'
+import ProgressBar from "./progress.jsx";
+import {AiOutlineHtml5} from 'react-icons/ai'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <div className='experience'>
+
+        <h5>Skills</h5>
+        <h2>Experience</h2>
+
+        <div className="contenedor c_experience">
+          <div className="e_left">
+
+            <div className='e_language'>
+              <AiOutlineHtml5 className='e_language-icons'/>
+              <div class="skill">
+                  <p>HTML</p>
+                  <div class="skill-bar skill1">
+                      <span class="skill-count1">Advanced</span>
+                  </div>
+              </div>
+            </div>
+
+            <div className='e_language'>
+              <AiOutlineHtml5 className='e_language-icons'/>
+              <div class="skill">
+                  <p>CSS</p>
+                  <div class="skill-bar skill1">
+                      <span class="skill-count1">Advanced</span>
+                  </div>
+              </div>
+            </div>
+
+            <div className='e_language'>
+              <AiOutlineHtml5 className='e_language-icons'/>
+              <div class="skill">
+                  <p>JS</p>
+                  <div class="skill-bar skill1">
+                      <span class="skill-count1">Advanced</span>
+                  </div>
+              </div>
+            </div>
+
+            <div className='e_language'>
+              <AiOutlineHtml5 className='e_language-icons'/>
+              <div class="skill">
+                  <p>REACT</p>
+                  <div class="skill-bar skill1">
+                      <span class="skill-count1">Advanced</span>
+                  </div>
+              </div>
+            </div>
+
+
+
+
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/experience/progress.jsx
@@ -0,0 +1,33 @@
+const ProgressBar = (props) => {
+  const { bgcolor, completed } = props;
+
+  const containerStyles = {
+    width: 100,
+    backgroundColor: "#e0e0de",
+    borderRadius: 50
+  }
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 'inherit',
+    textAlign: 'right'
+  }
+
+  const labelStyles = {
+    padding: 0.1,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}></span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
