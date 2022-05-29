04724716b255f55b9e00a628edcc2d098da44da3
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-29 08:09:38 -0600

    Nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {GoBook} from 'react-icons/go'
+import {GoBriefcase} from 'react-icons/go'
+import {AiOutlineMessage} from 'react-icons/ai'
+
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome size={20}/></a>
+      <a href='#about'><AiOutlineUser size={20}/></a>
+      <a href='#experience'><GoBook size={20}/></a>
+      <a href='#portafolio'><GoBriefcase size={20}/></a>
+      <a href='#contact'><AiOutlineMessage size={20}/></a>
+    </nav>
   )
 }
 
