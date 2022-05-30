bd96f560dbceecdcd19e41a6c92251797a43931b
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-30 09:33:54 -0600

    FOOTER [DONE]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      Stefano Aragoni
+      <div className="f_horizontal">
+        <a href='https://github.com/stefanoaragoni' target={"_blank"}><BsGithub size={30}/></a>
+        <a href='https://linkedin.com/in/stefanoaragoni' target={"_blank"}>< BsLinkedin size={30}/></a>
+      </div>
+    </footer>
   )
 }
 
